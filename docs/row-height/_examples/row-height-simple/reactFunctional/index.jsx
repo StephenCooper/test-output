34e71a44b84f2305de884c19102c00e116003035
Stephen Cooper <scooperdev@gmail.com>
2025-02-18 17:29:23 +0000

    jsx fixes

--- docs/row-height/_examples/row-height-simple/reactFunctional/index.jsx
+++ docs/row-height/_examples/row-height-simple/reactFunctional/index.jsx
@@ -16,15 +16,11 @@ ModuleRegistry.registerModules([
   ClientSideRowModelModule,
   ValidationModule /* Development Only */,
 ]);
-import { useFetchJson } from "./useFetchJson";
 
 const GridExample = () => {
-  const { data, loading } = useFetchJson(
-    "https://www.ag-grid.com/example-assets/olympic-winners.json",
-  );
   const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
   const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
-
+  const [rowData, setRowData] = useState();
   const [columnDefs, setColumnDefs] = useState([
     { field: "rowHeight" },
     { field: "athlete" },
@@ -45,6 +41,18 @@ const GridExample = () => {
     };
   }, []);
 
+  const onGridReady = useCallback((params) => {
+    fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .then((resp) => resp.json())
+      .then((data) => {
+        const differentHeights = [40, 80, 120, 200];
+        data.forEach(function (dataItem, index) {
+          dataItem.rowHeight = differentHeights[index % 4];
+        });
+        setRowData(data);
+      });
+  }, []);
+
   const getRowHeight = useCallback((params) => {
     return params.data.rowHeight;
   }, []);
@@ -53,11 +61,11 @@ const GridExample = () => {
     <div style={containerStyle}>
       <div style={gridStyle}>
         <AgGridReact
-          rowData={data}
-          loading={loading}
+          rowData={rowData}
           columnDefs={columnDefs}
           defaultColDef={defaultColDef}
           getRowHeight={getRowHeight}
+          onGridReady={onGridReady}
         />
       </div>
     </div>
